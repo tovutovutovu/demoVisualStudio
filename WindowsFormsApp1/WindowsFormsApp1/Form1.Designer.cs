5a3816cbda84c7ca8eff3628ef2d662785252c4e
tovu2021 <94627804+tovu2021@users.noreply.github.com>
2021-11-18 22:27:32 +0700

    Add files via upload

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -34,11 +34,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(647, 386);
+            this.button1.Location = new System.Drawing.Point(558, 276);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 52);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "TOVU2021";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // treeView1
